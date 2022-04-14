1e73bd00e5477db774e7f9addf895551d475e033
francelino <fmarcilio1989@gmail.com>
2022-04-13 21:50:43 -0300

    service de calculadora

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,15 +1,13 @@
+import { CalculadoraService } from './calculadora.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
-  styleUrls: ['./calculadora.component.css']
+  styleUrls: ['./calculadora.component.css'],
 })
 export class CalculadoraComponent implements OnInit {
+  constructor(private calculadoraService: CalculadoraService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,15 +1,12 @@
+import { CalculadoraService } from './calculadora.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from './calculadora.component';
 
-
-
 @NgModule({
-  declarations: [
-    CalculadoraComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CalculadoraComponent],
+  imports: [CommonModule],
+  exports: [CalculadoraComponent],
+  providers: [CalculadoraService],
 })
-export class CalculadoraModule { }
+export class CalculadoraModule {}
