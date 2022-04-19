e795159bbc405f86d85783393fe55447e0e473e7
francelino <fmarcilio1989@gmail.com>
2022-04-18 21:38:45 -0300

    implementando o component de calculadora

--- src/app/calculadora/calculadora.service.spec.ts
+++ src/app/calculadora/calculadora.service.spec.ts
@@ -1,6 +1,5 @@
-import { inject, TestBed } from '@angular/core/testing';
-
 import { CalculadoraService } from './calculadora.service';
+import { inject, TestBed } from '@angular/core/testing';
 
 describe('CalculadoraService', () => {
   let service: CalculadoraService;
@@ -21,6 +20,14 @@ describe('CalculadoraService', () => {
     (service: CalculadoraService) => {
       let soma = service.calcular(1, 4, CalculadoraService.SOMA);
       expect(soma).toEqual(5);
+    }
+  ));
+
+  it('Deve garantir que 1 - 4 = -3', inject(
+    [CalculadoraService],
+    (service: CalculadoraService) => {
+      let subtracao = service.calcular(1, 4, CalculadoraService.SUBTRACAO);
+      expect(subtracao).toEqual(-3);
     }
   ));
 });
